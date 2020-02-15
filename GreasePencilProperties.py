433060524c107108870aef0d459a591da058d55a
df-0 <dave@rgl.tv>
2020-02-15 20:19:05 +0000

    sorta works

--- GreasePencilProperties.py
+++ GreasePencilProperties.py
@@ -12,17 +12,9 @@ from enum import Enum
 
 class GreasePencilCustomProperties(bpy.types.PropertyGroup):
     """Scene properties that are used for the addon"""
-    #camera: bpy.props.StringProperty() = ""
-    #panelExpanded_setup: bpy.props.BoolProperty() = True
-    #panelExpanded_draw: bpy.props.BoolProperty() = False
-    #panelExpanded_solve: bpy.props.BoolProperty() = False
     currentMode: bpy.props.IntProperty() = 0
     previousMode: bpy.props.IntProperty() = 0  # so we can see if it changed
     layerNames: bpy.props.StringProperty() = [] # should really be a list but ain't nobody got time for that
-    #customLayers: bpy.props.CollectionProperty() #= [1,2,3]
-    #modes: bpy.props.EnumProperty()
-    #solve_respectLockedLayers: bpy.props.BoolProperty() = True
-    #solve_respectHiddenLayers: bpy.props.BoolProperty() = True
 
 
 class OBJECT_PT_greasePencilCustomPanel(bpy.types.Panel):  
@@ -47,6 +39,11 @@ class OBJECT_PT_greasePencilCustomPanel(bpy.types.Panel):
             row.label(text=layerNames[currentMode])
         
 
+def hasLength(v):
+    """ does it have a length attribute? """
+    return ('__len__' in dir(v))
+
+
 def addModeProperties():
     """Adds an enum to the UI that controls the grease pencil display mode"""
     viewLayers = bpy.context.scene.view_layers.values()
@@ -62,6 +59,73 @@ def getAllGreasePencilMaterials():
         if mat.is_grease_pencil:
             answer.append(mat)
     return answer
+
+
+def setMaterialColor(mat, color):
+    # make sure the color is 3 digit
+    if hasLength(color) is False:
+        # it has no length attribute, gonna put it in a list
+        color = [color, 0, 0]
+    elif len(color)<3:
+        color += [0, 0, 0]
+        color = color[:3]
+    # ok now color should be a list of 3 digits
+
+    if mat.grease_pencil.show_fill:
+        # set fill
+        mat.grease_pencil.fill_color[0] = color[0]
+        mat.grease_pencil.fill_color[1] = color[1]
+        mat.grease_pencil.fill_color[2] = color[2]
+    if mat.grease_pencil.show_stroke:  # note: can do both!
+        # set stroke
+        mat.grease_pencil.color[0] = color[0]
+        mat.grease_pencil.color[1] = color[1]
+        mat.grease_pencil.color[2] = color[2]
+
+
+def getMaterialColor(mat):
+    # first figure out appropriate vlaue
+    if mat.grease_pencil.show_stroke:
+        return [mat.grease_pencil.color[0], mat.grease_pencil.color[1], mat.grease_pencil.color[2]]
+    elif mat.grease_pencil.show_fill:
+        return [mat.grease_pencil.fill_color[0], mat.grease_pencil.fill_color[1], mat.grease_pencil.fill_color[2]]
+    return [0, 0, 0]
+
+
+def updateColorToCurrentMode():
+    """ stores the previous tweaks, set the color from the other thing """
+    props = bpy.context.scene.grease_pencil_custom_properties
+    currentMode = props.currentMode
+    previousMode = props.previousMode
+    layerNames = props.layerNames.split()
+
+    if currentMode == previousMode:
+        return False # didn't need to update
+    
+    print('switching from mode', layerNames[previousMode], 'to', layerNames[currentMode])
+
+    # for all mats
+    for mat in getAllGreasePencilMaterials():
+        # store old value
+        color = getMaterialColor(mat)
+        storeLocation = mat[layerNames[previousMode]]
+        if hasLength(storeLocation):
+            for i in range(len(storeLocation)):
+                if i>=3:
+                    break
+                storeLocation[i] = color[i]
+        else:
+            mat[layerNames[previousMode]] = color[0] # just set first value
+
+        pass
+
+
+        # load new value
+        setMaterialColor(mat, mat[layerNames[currentMode]])
+
+    # update modes
+    props.previousMode = currentMode
+
 
 
 def addCustomProperty(propertyName, defaultValue):
@@ -80,10 +144,7 @@ def addCustomProperty(propertyName, defaultValue):
         if propertyName not in mat.keys():
             # special case for colour
             if propertyName == "param_color":
-                if mat.grease_pencil.show_stroke:
-                    defaultValue = [mat.grease_pencil.color[0], mat.grease_pencil.color[1], mat.grease_pencil.color[2]]
-                elif mat.grease_pencil.show_fill:
-                    defaultValue = [mat.grease_pencil.fill_color[0], mat.grease_pencil.fill_color[1], mat.grease_pencil.fill_color[2]]
+                defaultValue = getMaterialColor(mat)
 
             # ok, it doesn't exist, let's add it
             mat[propertyName] = defaultValue
@@ -113,11 +174,13 @@ def setup():
     # add all the params
     props = [
                 ('param_color', [0.5, 0.5, 0.5]),
-                ('param_specularSmooth', 0),
-                ('param_specularRough', 0)
+                ('param_specularSmooth', 0.0),
+                ('param_specularRough', 0.0)
             ]
     for p in props:
         addCustomProperty(p[0], p[1])
+
+    addModeProperties()
 
     print('completed setup')
     return
@@ -125,4 +188,5 @@ def setup():
 
 setup()
 
-addModeProperties()
+
+#updateColorToCurrentMode()
